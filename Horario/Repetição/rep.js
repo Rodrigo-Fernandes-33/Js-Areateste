c7941ee8994260b92facb0ed8e2b9a55b598b0af
Rodrigo-Fernandes-33 <rfcode.33@gmail.com>
2025-01-21 00:31:07 -0400

    Contagem

--- Horario/Repetição/rep.js
+++ Horario/Repetição/rep.js
@@ -1,3 +1,20 @@
-for (c=1;c<=10;c++) {
-    console.log(c)
+function Calcular () {
+    let inicio = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO!] Faltam Dados!')
+    } else {
+        res.innerHTML = 'Contando'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (i < f) {
+        for (let c = i; c <= f; c+=p) {
+            res.innerHTML += `\u{1F449} ${c} `
+        }
+    }
+    res.innerHTML += `\u{1F3C1}`
+    }
 }
