67cd25abf5e989abd85bfef0ccec193b019096a1
Rodrigo-Fernandes-33 <rfcode.33@gmail.com>
2025-01-20 22:47:47 -0400

    Inclusão de campo de imagem

--- Identificador de Idade/scriptverid.js
+++ Identificador de Idade/scriptverid.js
@@ -8,6 +8,33 @@ function verificar() {
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade}`
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        var genero = ''
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >=0 && idade <10) {
+                img.setAttribute('src','Homem Novo.jpg')
+                } else if (idade < 32) {
+                img.setAttribute('src','Homem.jpg')
+                } else if (idade < 50) {
+                img.setAttribute('src', 'Homem.jpg')    
+                } else {
+                img.setAttribute('src', 'Homem Velho.jpg')
+                }
+            } else if (fsex [1].checked) {
+            genero = 'Mulher'
+            if (idade >=0 && idade <10) {
+                img.setAttribute('src','Mulher Nova.jpg')
+                } else if (idade < 32) {
+                img.setAttribute('src', 'Mulher.jpg')    
+                } else if (idade < 50) {
+                img.setAttribute('src', 'Mulher.jpg')
+                } else {
+                img.setAttribute('src', 'Mulher Velha.jpg')
+                }
+            }
+            res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+            res.appendChild(img)
     }
 }
