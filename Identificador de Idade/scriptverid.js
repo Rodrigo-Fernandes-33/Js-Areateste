b33815ef3428954fcba875bd28386291f2daa901
Rodrigo-Fernandes-33 <rfcode.33@gmail.com>
2025-01-20 22:25:42 -0400

    Correções Verid

--- Identificador de Idade/scriptverid.js
+++ Identificador de Idade/scriptverid.js
@@ -1,12 +1,13 @@
-function verificar()
-    ver data = new Date()
-    ver ano = data.getFullYear()
-    ver fano = document.getElementById('txtano')
-    ver res = document.querySelector('div#res')
-    if (fano.value.lenght == 0 || Number(fano.value) > ano) {
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[Erro!] Verifique os dados e tente novamente!')
     } else {
         var fsex = document.getElementsByName('radsex')
-        var idade = ano - (fano.value)
+        var idade = ano - Number(fano.value)
         res.innerHTML = `Idade calculada: ${idade}`
+    }
 }
